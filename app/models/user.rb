d834c44fb4cf670fb5a1edb883dc4d6820e6e6d7
Alamgir <ourandoc@gmail.com>
2021-04-07 13:19:38 +0500

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 2 }
-    validates :email, presence: true, length: { maximum: 25 }
-    validates :password, length: { in: 6..20 }
+  validates :username, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, length: { maximum: 25 }
+  validates :password, length: { in: 6..20 }
 end
